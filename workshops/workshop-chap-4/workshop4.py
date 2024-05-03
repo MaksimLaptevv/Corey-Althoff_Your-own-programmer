ee5a003226687ee8463dec91c9f411320995c6dc
Maksim <165895340+MaksimLaptevv@users.noreply.github.com>
2024-05-03 22:26:07 +0800

    Update workshop4.py

--- workshops/workshop-chap-4/workshop4.py
+++ workshops/workshop-chap-4/workshop4.py
@@ -4,17 +4,8 @@ def func_one(x):  # entering an integer parameter x and dividing it by 2
 def func_two(y):  # entering an integer parameter y and multiplying it by 4
     return y*4
 
-b=0
+a = int(input())
 
-while b==0:       # loop for correct number entry
-    try:
-        a=int(input('enter num: '))
-        b+=1
-    except ValueError:
-        print('enter int num!!')
-
-x=func_one(a)
-print(a,'/',2,'=',x)
-
-y=func_two(x)
-print(x,'*',4,'=',y)
+y = func_one(a)
+x = func_two(y)
+print(x)
