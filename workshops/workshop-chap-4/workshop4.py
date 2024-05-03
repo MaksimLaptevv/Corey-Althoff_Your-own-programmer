61ccca73068d472bfa03c06e617468d20fc1e1c1
Maksim <165895340+MaksimLaptevv@users.noreply.github.com>
2024-05-03 22:59:00 +0800

    Update workshop4.py

--- workshops/workshop-chap-4/workshop4.py
+++ workshops/workshop-chap-4/workshop4.py
@@ -1,5 +1,5 @@
 def func_one(x):  # entering an integer parameter x and dividing it by 2
-    return x//2
+    return x/2
 
 def func_two(y):  # entering an integer parameter y and multiplying it by 4
     return y*4
